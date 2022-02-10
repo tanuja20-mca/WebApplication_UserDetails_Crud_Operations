600b3c2a94fa355cc601e52d73d7093b50886fe4
Tanuja pandey <pandey.tanuja2015@gmail.com>
2022-02-10 16:40:12 +0530

    Add project files.

---
+++ WebApplication_FormLogin/Login.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Login.aspx.cs" Inherits="WebApplication_FormLogin.Login" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblFirstName" runat="server" Text="First Name :"></asp:Label>
+            <asp:TextBox ID="txtFirstName" runat="server"></asp:TextBox>
+            <br />
+               <asp:Label ID="lblLastName" runat="server" Text="Last Name :"></asp:Label>
+            <asp:TextBox ID="txtLastName" runat="server"></asp:TextBox>
+            <br />
+               <asp:Label ID="lblCity" runat="server" Text="City :"></asp:Label>
+            <asp:TextBox ID="txtCity" runat="server"></asp:TextBox>
+            <br />
+
+            <asp:Button ID="btnInsert" runat="server" Text="Insert" OnClick="btnInsert_Click" />
+            <asp:Button ID="btnUpdate" runat="server" Text="Update" OnClick="btnUpdate_Click" />
+            <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" />
+            <asp:Button ID="btnSelect" runat="server" Text="Select" OnClick="btnSelect_Click" />
+            <br />
+            <br />
+            <br />
+            <asp:Label ID="lblID" runat="server" Text="Enter Id for Data Updation:"></asp:Label>
+            <asp:TextBox ID="txtID" runat="server"></asp:TextBox>
+            <br />
+            <br />
+            <asp:GridView ID="GridView1" runat="server">
+            </asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ WebApplication_FormLogin/Login.aspx.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication_FormLogin
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        SqlConnection con = new SqlConnection("Data Source=.;Initial Catalog=FormLoginDetails;Integrated Security=True");
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(con.State== System.Data.ConnectionState.Open)
+            {
+                con.Close();
+            }
+            con.Open();
+           display_Data();
+           
+
+        }
+
+        protected void btnInsert_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("insert into Login values('" + txtFirstName.Text + " ','" + txtLastName.Text + "','" + txtCity.Text + "')",con);
+            cmd.ExecuteNonQuery();
+
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtCity.Text = "";
+
+           display_Data();
+        
+        }
+
+        protected void btnUpdate_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = new SqlCommand("update Login set FirstName= '" + txtFirstName.Text + " ', LastName='" + txtLastName.Text + "', City='" + txtCity.Text + "' where id=" + Convert.ToInt32(txtID.Text) + "" ,con);
+            cmd.ExecuteNonQuery();
+
+            txtFirstName.Text = "";
+            txtLastName.Text = "";
+            txtCity.Text = "";
+
+           display_Data();
+
+        }
+
+        protected void btnDelete_Click(object sender, EventArgs e)
+        {
+            SqlCommand cmd = con.CreateCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.CommandText = "delete from Login where FirstName= '" + txtFirstName.Text + " '";
+            cmd.ExecuteNonQuery();
+
+            txtFirstName.Text = "";
+        
+           display_Data();
+
+
+        }
+
+        protected void btnSelect_Click(object sender, EventArgs e)
+        {
+            display_Data();
+
+        }
+         public void display_Data()
+        {
+            
+             SqlCommand cmd = new SqlCommand("select * from Login",con) ;
+
+             cmd.ExecuteNonQuery();
+             DataTable dt = new DataTable();
+             SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+             adapter.Fill(dt);
+             GridView1.DataSource = dt;
+             GridView1.DataBind();
+
+
+        }  
+    }
+}
